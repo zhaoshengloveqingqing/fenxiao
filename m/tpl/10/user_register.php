b43a37a0c30f3e3c51aeec00d0979974a1c7bcc7
lmjean <jean.lu@pinet.co>
2015-07-16 17:17:11 +0800

    add the login and register

--- m/tpl/10/user_register.php
+++ m/tpl/10/user_register.php
@@ -1,45 +1,22 @@
-<link type="text/css" rel="stylesheet" href="<?php echo ADMIN_URL;?>tpl/15/css.css" media="all" />
-<?php $this->element('15/top',array('lang'=>$lang)); ?>
-<style type="text/css">
-.pw{
-border: 1px solid #ddd;
-border-radius: 5px;
-background-color: #fff; padding-left:5px; padding-right:5px;
--moz-border-radius:5px;/*仅Firefox支持，实现圆角效果*/
--webkit-border-radius:5px;/*仅Safari,Chrome支持，实现圆角效果*/
--khtml-border-radius:5px;/*仅Safari,Chrome支持，实现圆角效果*/
-border-radius:5px;/*仅Opera，Safari,Chrome支持，实现圆角效果*/
-}
-</style>
-<div id="main" style="padding:10px; min-height:300px">
-	<form id="REGISTER1" name="REGISTER1" method="post" action="">
-			<table cellpadding="3" cellspacing="5" border="0" width="100%">
-			<tr>
-				<th align="left">手机号码：</th>
-			</tr>
-			<tr>
-				<td width="100%" align="center"><input type="text" name="mobile_phone" style="width:95%; height:30px; line-height:normal;" class="pw"/></td>
-			</tr>
-			<tr>
-				<th align="left">用户密码：</th>
-			</tr>
-			<tr>
-				<td width="100%" align="center"><input type="password" name="password" style="width:95%; height:30px; line-height:normal;" class="pw"/></td>
-			</tr>
-			<tr>
-				<th align="left">确认密码：</th>
-			</tr>
-			<tr>
-				<td width="100%" align="center"><input type="password" name="rp_pass" style="width:95%; height:30px; line-height:normal;" class="pw"/></td>
-			</tr>
-			<tr>
-				<td align="center" width="100%"><input type="hidden" name="user_rank" value="1" />
-				<input name="" value="同意协议并注册" type="button" id="submit" tabindex="6" data-disabled="false" style=" padding:5px; background:#FEC627; color:#fff;width:100%; line-height:25px; cursor:pointer; font-weight:bold; border:none" class="pw" onclick="return submit_register_data('REGISTER1');" />
-			  </td>
-			</tr>
-			</table> 
-			 </form>
-		
+<link type="text/css" rel="stylesheet" href="<?php echo ADMIN_URL;?>tpl/10/css/top_bottom.css<?php echo '?'.time();?>" media="all" />
+<link type="text/css" rel="stylesheet" href="<?php echo ADMIN_URL;?>tpl/10/css/user_register.css<?php echo '?'.time();?>" media="all" />
+<?php $this->element('10/top',array('lang'=>$lang)); ?>
+<div class="register_main">
+	<form id="register">
+		<div class="am-form-group">
+			<label for="doc-ipt-email-1">手机帐号:</label>
+			<input type="email" class="" id="doc-ipt-email-1" placeholder="11位手机号码">
+		</div>
+		<div class="am-form-group">
+			<label for="doc-ipt-email-1">用户密码:</label>
+			<input type="password" class="" id="doc-ipt-email-1" placeholder="输入密码">
+		</div>
+		<div class="am-form-group">
+			<label for="doc-ipt-email-1">确认密码:</label>
+			<input type="password" class="" id="doc-ipt-email-1" placeholder="再次输入密码">
+		</div>
+		<input value="同意协议并注册" type="button" id="submit" />
+	</form>
 </div>
-<?php $this->element('15/footer',array('lang'=>$lang)); ?>
+<?php $this->element('10/footer',array('lang'=>$lang)); ?>
 
