ef6482bc342884594f3e9bc5010c4e282a40e6ac
lmjean <jean.lu@pinet.co>
2015-07-21 10:29:36 +0800

    change the style

--- m/tpl/10/mycart_checkout_empty.php
+++ m/tpl/10/mycart_checkout_empty.php
@@ -1,11 +1,9 @@
 <link type="text/css" rel="stylesheet" href="<?php echo ADMIN_URL;?>tpl/10/css/checkout_empty.css" media="all" />
-
-	<div id="main">
-			<div class="ctop"></div>
-				<p>购物车还空着</p>
-				<p>去选几件中意的商品吧</p>
-			<p>
-				<a style="width:80px; height:30px; line-height:30px; border-radius:5px; border:1px solid #b8b8b8; display:block; margin:0px auto; font-size:18px; text-decoration:underline" href="<?php echo ADMIN_URL;?>">去逛逛</a>
-			</p>
-
-	</div>
+<div id="main">
+	<div class="ctop"></div>
+	<p>购物车还空着</p>
+	<p>去选几件中意的商品吧</p>
+	<p>
+		<a style="width:80px; height:30px; line-height:30px; border-radius:5px; border:1px solid #b8b8b8; display:block; margin:0px auto; font-size:18px; text-decoration:underline" href="<?php echo ADMIN_URL;?>">去逛逛</a>
+	</p>
+</div>
