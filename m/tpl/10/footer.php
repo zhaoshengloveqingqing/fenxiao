f35e3ae7655131ed669256fe9277e465cd86e92c
JasonPinet <jason.hu@pinet.co>
2015-07-21 11:35:22 +0800

    category info

--- m/tpl/10/footer.php
+++ m/tpl/10/footer.php
@@ -1,3 +1,22 @@
+
+<div id="opquyubox">
+	<!--<p><img src="<?php echo $this->img('homeMenuTop.png');?>" width="100%" /></p>-->
+	<div style="line-height:26px;">
+	<!--	<h2 style="border-bottom:1px solid #ededed;"><a href="<?php echo ADMIN_URL.'exchange.php';?>">积分兑换</a></h2>-->
+	<?php if(!empty($lang['menu']))foreach($lang['menu'] as $row){?>
+		<h2 style="border-bottom:1px solid #ededed;"><a href="<?php echo ADMIN_URL.'catalog.php?cid='.$row['id'];?>"><?php echo $row['name'];?></a></h2>
+		<?php if(!empty($row['cat_id'])){?>
+		<div style=" line-height:14px; padding-left:15px">
+			<?php foreach($row['cat_id'] as $rows){?>
+			<a href="<?php echo ADMIN_URL.'catalog.php?cid='.$rows['id'];?>"><?php echo $rows['name'];?></a>
+			<?php } ?>
+		</div>
+	<?php } } ?>
+	</div>
+	<div style=" height:45px;"></div>
+</div>
+
+
 <div id="footer">
 	<nav>
 		<ul id="menu" class="top_menu">
