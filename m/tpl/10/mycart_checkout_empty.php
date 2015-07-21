0cc0cee71eace1eba04bbde3b9b212b03e86d0f7
lmjean <jean.lu@pinet.co>
2015-07-21 11:48:39 +0800

    add the checkout_empty

--- m/tpl/10/mycart_checkout_empty.php
+++ m/tpl/10/mycart_checkout_empty.php
@@ -3,7 +3,7 @@
 	<div class="ctop"></div>
 	<p>购物车还空着</p>
 	<p>去选几件中意的商品吧</p>
-	<p>
-		<a style="width:80px; height:30px; line-height:30px; border-radius:5px; border:1px solid #b8b8b8; display:block; margin:0px auto; font-size:18px; text-decoration:underline" href="<?php echo ADMIN_URL;?>">去逛逛</a>
+	<p class="action">
+		<a class="button" href="<?php echo ADMIN_URL;?>">去逛逛</a>
 	</p>
 </div>
