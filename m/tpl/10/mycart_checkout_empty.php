d98a97b9bf32dbb27716f0246f1dbf5a4421a4f7
lmjean <jean.lu@pinet.co>
2015-07-22 16:19:10 +0800

    add the order_details

--- m/tpl/10/mycart_checkout_empty.php
+++ m/tpl/10/mycart_checkout_empty.php
@@ -1,7 +1,10 @@
 <link type="text/css" rel="stylesheet" href="<?php echo ADMIN_URL;?>tpl/10/css/top_bottom.css<?php echo '?'.time();?>" media="all" />
 <link type="text/css" rel="stylesheet" href="<?php echo ADMIN_URL;?>tpl/10/css/checkout_empty.css" media="all" />
+<?php $this->element('10/top',array('lang'=>$lang)); ?>
 <div id="main">
-	<div class="ctop"></div>
+	<div class="ctop">
+		<img src="<?php echo ADMIN_URL;?>tpl/10/images/gouwuche_icon.png"/>
+	</div>
 	<p>购物车还空着</p>
 	<p>去选几件中意的商品吧</p>
 	<p class="action">
