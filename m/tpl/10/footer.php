557eb2a41479e0daa2f1d1e78a44b81841cad2ad
lmjean <jean.lu@pinet.co>
2015-08-17 17:26:48 +0800

    change the bug

--- m/tpl/10/footer.php
+++ m/tpl/10/footer.php
@@ -2,7 +2,7 @@
 <div id="opquyubox">
 	<div  style="height: 100%;">
 		<h2 style="text-align: left;">商品分类</h2>
-		<ul class="menu" style="overflow-y: scroll; text-align: left; display: block;  margin-bottom: 75px !important;height: 100%;">
+		<ul class="menu" style="overflow-y: scroll; text-align: left; display: block; height: 100%;">
 			<?php if(!empty($lang['menu']))foreach($lang['menu'] as $row){?>
 			<li>
 				<a href="<?php echo ADMIN_URL.'catalog.php?cid='.$row['id'];?>">
