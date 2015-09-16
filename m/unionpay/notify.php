a72aea3a72b95020c7e1902fd256d89bb5f3271d
JasonPinet <jason.hu@pinet.co>
2015-09-16 17:38:02 +0800

    unionpay notice url

--- m/unionpay/notify.php
+++ m/unionpay/notify.php
@@ -4,6 +4,5 @@ $res = $_POST;
 file_put_contents('/var/www/pinet-fenxiao/cache/unionpay', print_r($res, 1).PHP_EOL, FILE_APPEND);
 if ($res['respCode'] == '00' && $res['respMsg'] = 'success') {
 	$app->action('shopping','pay_successs_tatus2',array('order_sn'=>$res['orderId'], 'status'=>'1'));//修改支付状态
-}else{
-	$this->jump(str_replace('/unionpay','',ADMIN_URL).'user.php?act=orderlist');exit;
 }
+$this->jump(str_replace('/unionpay','',ADMIN_URL).'user.php?act=orderlist');exit;
