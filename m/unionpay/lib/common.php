450f7b4e13fe714158c1a061ea5c1fdbbfe45fb2
JasonPinet <jason.hu@pinet.co>
2015-09-16 17:45:19 +0800

    unionpay remove submit button

--- m/unionpay/lib/common.php
+++ m/unionpay/lib/common.php
@@ -148,7 +148,6 @@ eot;
 		$html .= "    <input type=\"hidden\" name=\"{$key}\" id=\"{$key}\" value=\"{$value}\" />\n";
 	}
 	$html .= <<<eot
-    <input type="submit" type="hidden">
     </form>
 </body>
 </html>
