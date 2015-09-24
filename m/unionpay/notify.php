db98c07db841e0d7e93a1303ac38355941ed9ce8
JasonPinet <jason.hu@pinet.co>
2015-09-24 16:42:32 +0800

    add log for unionpay

--- m/unionpay/notify.php
+++ m/unionpay/notify.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../load.php');
 $res = $_POST;
-file_put_contents('/var/www/pinet-fenxiao/cache/unionpay', print_r($res, 1).PHP_EOL, FILE_APPEND);
+file_put_contents('/var/www/pinet-fenxiao/cache/unionpay/notify', print_r($res, 1).PHP_EOL, FILE_APPEND);
 if ($res['respCode'] == '00' && $res['respMsg'] = 'success') {
 	$app->action('shopping','pay_successs_tatus2',array('order_sn'=>$res['orderId'], 'status'=>'1'));//修改支付状态
 }
